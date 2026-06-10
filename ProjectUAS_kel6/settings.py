187aa27702db32494003f5b16828c8000582c95b
Aulia Neha Iswantoro <2411102441121@umkt.ac.id>
2026-06-10 14:37:51 +0800

    nyalakan debug untuk cek error database

--- ProjectUAS_kel6/settings.py
+++ ProjectUAS_kel6/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6*n3@i1#$ov_cf##r&looia-*2b+@6+n+geem6iip5y#ove27l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
